ad3b57080ba0b361336d64dce8dacd48efe61534
Narine Boyakhchyan <narine.boyakhchyan@gmail.com>
2017-06-09 12:21:22 +0400

    Create App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import AdminDashboard from './AdminDashboard.js';
 import './StyleSheet/App.css';
-class App extends Component {
 
+export default class App extends Component {
   render() {
     return (
       <div className="global_container" >
-
                     <AdminDashboard/>
-
-          </div>
+      </div>
     );
   }
 }
 
-export default App;
