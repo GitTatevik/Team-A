70c072d206e2393d4c7003381f816ae9788b178d
Narine Boyakhchyan <narine.boyakhchyan@gmail.com>
2017-06-09 12:21:55 +0400

    Create App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="global_container" >
-                    <AdminDashboard/>
+        <AdminDashboard/>
       </div>
     );
   }
