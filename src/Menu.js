b921d25fba4bdff904be22a30562cc6fdede8ba3
Narine Boyakhchyan <narine.boyakhchyan@gmail.com>
2017-06-09 12:26:07 +0400

    Create Menu.js

--- src/Menu.js
+++ src/Menu.js
@@ -2,18 +2,15 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './StyleSheet/Menu.css';
 
-class Menu extends Component{
+export default class Menu extends Component{
   render(){
     return(
           <div className="menu">
                   <ul className="list_menu">
                       <li className="menu_item"><NavLink activeClassName="active" to='/contacts'>Contacts</NavLink></li>
-                       <li className="menu_item"><NavLink activeClassName="active" to='/mailinglist'>Mailing list </NavLink></li>
-                 </ul>
+                      <li className="menu_item"><NavLink activeClassName="active" to='/mailinglist'>Mailing list </NavLink></li>
+                  </ul>
           </div>
-      
     );
   }
 }
-
-export default Menu;
